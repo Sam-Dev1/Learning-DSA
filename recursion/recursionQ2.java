55f47b2029791fb7e2e6e4643761af299c973436
Sam-Dev1 <samirkhatiwada154@gmail.com>
2026-06-09 15:12:48 +0545

    Added to reposatory

---
+++ recursion/recursionQ2.java
@@ -0,0 +1,16 @@
+//print number from 1 to 5 using recursion
+package recursion;
+
+public class recursionQ2 {
+    public static void main(String[] args) {
+        int n = 1;
+        printNumbers(n);
+    }
+    public static void printNumbers(int n){
+    	if(n==6){
+		return;
+	}
+	System.out.println(n);
+	printNumbers(n+1);
+    }
+}
